0cd1b576fff275b19b4e54b43740910d3cd08efa
Webster Fever <webstersevenlanguages@gmail.com>
2026-03-01 17:21:18 -0500

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello World");
+// Require the framework and instantiate it
+import "dotenv/config";
+
+// ESM
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
+
+try {
+  await fastify.listen({ port: process.env.PORT ?? 8080 });
+  fastify.log.info(`Server is running on port ${process.env.PORT ?? 8080}`);
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
+// Run the server!
